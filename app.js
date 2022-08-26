e1f1a98c1a2f4cf7bb40386d15a86b03a59382af
Elizabeth Kennedy <eakennedy.2412@gmail.com>
2022-08-25 23:04:36 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let min = 1;
+let max = 100;
+let rand = Math.floor(Math.random() * max) +1;
+let isPlaying = true
+
+while (isPlaying) {
+    turn()
+}
+    function turn() {
+    let input = Number.parseInt(prompt(`What is my favorite number between ${min} and ${max}?`)); 
+    if (input < rand) {
+        alert ("Too low. Try again.");
+        } else if (input > rand) {
+        alert ("Too high. Try again.");
+        } else if (input == rand) {
+        alert ("Congratulations!!!");
+        isPlaying = false;
+        }  else {
+        alert (`Please enter a valid number between ${min} and ${max}.`);
+        
+    }
+}
